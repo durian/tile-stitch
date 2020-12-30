a33a39b901d7c899151d9560341f24eaa1510350
Peter Berck <pberck@gmail.com>
2020-12-30 12:08:47 +0100

    Updated bash script to loop and use -c option.

--- make_map.sh
+++ make_map.sh
@@ -1,14 +1,38 @@
-#!/bin/sh
+#!/bin/bash
 #
+MAPN=ESTA
 TILE=http://a.tile.openstreetmap.org/{z}/{x}/{y}.png
 ZOOM=8
-NAME=map_TMP_z${ZOOM}.png
+SIZE=1200
+CLAT=56.292109
+CLON=12.854471
 
-echo ./stitch -o ${NAME} -x -- 56.0 12.4 56.5 13.5 ${ZOOM} ${TILE}
-./stitch -o ${NAME} -x -- 56.0 12.4 56.5 13.5 ${ZOOM} ${TILE}
+#echo ./stitch -o ${NAME} -x -- 56.0 12.4 56.5 13.5 ${ZOOM} ${TILE}
+#./stitch -o ${NAME} -x -- 56.0 12.4 56.5 13.5 ${ZOOM} ${TILE}
 
-# If installed we normalise the colours.
-if command -v mogrify >/dev/null 2>&1; then
-    echo mogrify -normalize $NAME
-    mogrify -normalize $NAME
-fi
+command -v ./stitch >/dev/null 2>&1 || {
+    echo >&2 "I require ./stitch!"
+    exit 1
+}
+
+function normalise {
+    if command -v mogrify >/dev/null 2>&1; then
+	echo mogrify -normalize "${1}"
+	mogrify -normalize "${1}"
+    fi
+}
+
+function stitch {
+    echo ./stitch -o $1 -x -c -- ${CLAT} ${CLON} ${SIZE} ${SIZE} $2 ${TILE}
+    ./stitch -o $1 -x -c -- ${CLAT} ${CLON} ${SIZE} ${SIZE} $2 ${TILE}
+}
+
+for ZOOM in 8 9 10 11 12 13 14 15; do
+    NAME=$( printf "map_${MAPN}_Z%02d.png" ${ZOOM} )
+    if [ ! -e ${NAME} ]; then
+	stitch ${NAME} ${ZOOM} 
+	normalise ${NAME}
+    else
+	echo "${NAME} exists!"
+    fi
+done
