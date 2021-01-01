f74dc244e1ddd9eca8099030b81373bcf8dc9829
Peter Berck <pberck@gmail.com>
2021-01-01 12:57:28 +0100

    Updated script.

--- make_map.sh
+++ make_map.sh
@@ -10,11 +10,14 @@ CLON=13.361064
 #echo ./stitch -o ${NAME} -x -- 56.0 12.4 56.5 13.5 ${ZOOM} ${TILE}
 #./stitch -o ${NAME} -x -- 56.0 12.4 56.5 13.5 ${ZOOM} ${TILE}
 
+# This script expects the stich binary to be in the same
+# folder.
 command -v ./stitch >/dev/null 2>&1 || {
     echo >&2 "I require ./stitch!"
     exit 1
 }
 
+# Post-processing.
 function normalise {
     if command -v mogrify >/dev/null 2>&1; then
 	#mogrify -normalize "${1}"
@@ -24,7 +27,7 @@ function normalise {
     fi
 }
 
-function stitch {
+function do_stitch {
     echo ./stitch -o $1 -x -c -- ${CLAT} ${CLON} ${SIZE} ${SIZE} $2 ${TILE}
     ./stitch -o $1 -x -c -- ${CLAT} ${CLON} ${SIZE} ${SIZE} $2 ${TILE}
 }
@@ -32,7 +35,7 @@ function stitch {
 for ZOOM in 8 10 12; do
     NAME=$( printf "map_${MAPN}_Z%02d.png" ${ZOOM} )
     if [ ! -e ${NAME} ]; then
-	stitch ${NAME} ${ZOOM} 
+	do_stitch ${NAME} ${ZOOM} 
 	normalise ${NAME}
     else
 	echo "${NAME} exists!"
