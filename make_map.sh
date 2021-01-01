ad6ce7953c5965fc9c51d8aa600916ea0c1f37e5
Peter Berck <pberck@gmail.com>
2021-01-01 12:53:27 +0100

    Misc. update to script.

--- make_map.sh
+++ make_map.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 #
-MAPN=ESTA
 TILE=http://a.tile.openstreetmap.org/{z}/{x}/{y}.png
-ZOOM=8
-SIZE=1200
-#CLAT=56.292109
-#CLON=12.854471
-MAPN=RH118
-CLAT=56.053821
-CLON=13.530892
+SIZE=1280
+#
+MAPN=ESMM
+CLAT=55.538745
+CLON=13.361064
 
 #echo ./stitch -o ${NAME} -x -- 56.0 12.4 56.5 13.5 ${ZOOM} ${TILE}
 #./stitch -o ${NAME} -x -- 56.0 12.4 56.5 13.5 ${ZOOM} ${TILE}
@@ -32,7 +29,7 @@ function stitch {
     ./stitch -o $1 -x -c -- ${CLAT} ${CLON} ${SIZE} ${SIZE} $2 ${TILE}
 }
 
-for ZOOM in 12 13 14 15 16 17 18 19; do
+for ZOOM in 8 10 12; do
     NAME=$( printf "map_${MAPN}_Z%02d.png" ${ZOOM} )
     if [ ! -e ${NAME} ]; then
 	stitch ${NAME} ${ZOOM} 
